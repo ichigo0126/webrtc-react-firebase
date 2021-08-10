5a22ff0b9c86d7566b275f97fe72e1e4b8c4aaed
酒巻昇馬 <ichigo@sakamakinoborubanoMacBook-Air.local>
2021-08-10 22:09:59 +0900

    permit audio ando video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import TouchRipple from '@material-ui/core/ButtonBase/TouchRipple';
 import React from 'react';
 
-function App() {
+const getMedia = async ()=>  {
+  const constraints = {audio: TouchRipple, video: true};
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch(err) {
+    /* エラーを処理 */
+  }
+}
+
+
+getMedia();
+
+const App = ()  =>{
   return <div>Hello, React!</div>;
 }
 
