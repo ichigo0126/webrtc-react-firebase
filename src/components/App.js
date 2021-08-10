9f5b817aa698b047c8020b3c8248148ec8c1fc9e
酒巻昇馬 <ichigo@sakamakinoborubanoMacBook-Air.local>
2021-08-11 05:38:26 +0900

    let's set remotePeerName

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ getMedia();
 const App = ()  =>{
   const [localPeerName, setLocalPeerName] = useState('');
   const [remotePeerName, setRemotePeerName] = useState('');
-
+  console.log({localPeerName, remotePeerName})
   return (
   <>
     <InputFormLocal 
@@ -28,6 +28,7 @@ const App = ()  =>{
     setLocalPeerName={setLocalPeerName}
     />
     <InputFormRemote 
+    localPeerName={localPeerName}
     remotePeerName={remotePeerName}
     setRemotePeerName={setRemotePeerName}
     />
