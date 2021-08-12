205a061fabb4dd76e6728f26ebebddaa02ce1d2b
酒巻昇馬 <ichigo@sakamakinoborubanoMacBook-Air.local>
2021-08-13 00:24:57 +0900

    re-render when you change rtcClient properties

---
+++ src/components/Video.Remote.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useRef} from("react");
+import Video from './Video';
+
+const VideoRemote = ({name}) => {
+    const videoRef = null;
+
+    return <Video isLocal={false} name={name} videoRef={videoRef} />
+};
+
+export default VideoRemote;
