71a1529d132cf68baa42a07d000788cbafef90c1
酒巻昇馬 <ichigo@sakamakinoborubanoMacBook-Air.local>
2021-08-13 19:02:20 +0900

    refine video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Card,
+  CardActionArea,
+  CardActions ,
+  CardContent,
+  Typography,
+  } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
 const Video = ({ isLocal, name, videoRef }) => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <Card className={classes.root}>
+      <CardActionArea>
       <video autoPlay muted={isLocal} ref={videoRef} />
-      <div>{name}</div>
-    </div>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions></CardActions>
+    </Card>
   );
-};
+}
 
 export default Video;
